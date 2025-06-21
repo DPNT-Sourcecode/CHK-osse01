d4bed812e377439740ed3db4507d810987a647a4
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 13:54:16 +0000

    Sat Jun 21 13:54:16 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,6 +2,6 @@
 class SumSolution:
     
     def compute(self, x, y):
-        if not (0 <= x <= 100) or (0 <= y <= 100):
-            raise Exception("Sorry, number must be between 0 and 100")
+        if not (0 <= x <= 100) or not (0 <= y <= 100):
+            raise Exception("Sorry, numbers must be between 0 and 100")
         return x + y
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -7,7 +7,18 @@ class TestSum():
         assert SumSolution().compute(1, 2) == 3
 
     def test_range(self):
-        with pytest.raises(Exception, match="Sorry, number must be between 0 and 100"):
-            SumSolution().compute(1,99)
+        with pytest.raises(Exception, match="Sorry, numbers must be between 0 and 100"):
+            SumSolution().compute(-1,101)
+
+    def test_x_value(self):
+        with pytest.raises(Exception, match="Sorry, numbers must be between 0 and 100"):
+            SumSolution().compute(-1, 99)
+
+    def test_y_value(self):
+        with pytest.raises(Exception, match="Sorry, numbers must be between 0 and 100"):
+            SumSolution().compute(2, 101)
+
+    def test_boundary_sum(self):
+        assert SumSolution().compute(0, 100)
 
 
