b8f7995e80249802cef693807ec8d40999a39140
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 14:12:55 +0000

    Sat Jun 21 14:12:55 UTC 2025

---
+++ test/solution_tests/FIZ/test_fizz_buzz_solution.py
@@ -0,0 +1,6 @@
+from lib.solutions.FIZ.fizz_buzz_solution import FizzBuzzSolution
+
+class TestFizzBuzz:
+    def test_fizz_buzz_logic(self):
+        fizz_buzz_logic = FizzBuzzSolution()
+        return fizz_buzz_logic.fizz_buzz(5)
