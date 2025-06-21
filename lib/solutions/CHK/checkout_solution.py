64c08f53cc3197d161d650996a1aeed306cae9e8
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 14:44:35 +0000

    Sat Jun 21 14:44:35 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,25 @@
 
 class CheckoutSolution:
 
+    def __init__(self):
+        self.prices = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15
+        }
+
+        self.offers = {
+            "A": (3, 130),
+            "B": (2, 45)
+        }
+
     # skus = unicode string
-    def checkout(self, skus):
-        raise NotImplementedError()
+    def checkout(self, skus) -> int:
+        if not skus.isalpha():
+            return -1
+        
+
+
+
+
