8a4493177dfd1ed909391a4e4febe93c0ded5998
sbx_user1051 <sbx_user1051@169.254.16.215>
2025-06-21 21:06:53 +0000

    Sat Jun 21 21:06:53 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -52,15 +52,13 @@ class CheckoutSolution:
     def apply_free_items(self, items):
         for purchased_item, (free_item, offer_qty) in self.free_items.items():
             if purchased_item in items:
-                #i.e. 6 // 3 = 2 free items
-                if free_item in items:
-                    if purchased_item == free_item:
-                        size = offer_qty + 1 #how many you actually need to get one free and so
-                        free = items[purchased_item] // size
-                        items[purchased_item] -= free
-                    else:
-                        free_qty = items[purchased_item] // offer_qty
-                        items[free_item] = max(0, items[free_item] - free_qty)
+                count = items[purchased_item]
+                if purchased_item == free_item:
+                    free = count // offer_qty + 1 #how many you actually need to get one free and so
+                else:
+                    free = count // offer_qty
+                    if free_item in items:
+                        items[free_item] = max(0, items[free_item] - free)
 
     #Apply discounted items
     def apply_discount(self, items):
@@ -88,6 +86,7 @@ class CheckoutSolution:
         total = self.apply_discount(items)
 
         return total
+
 
 
 
