95ec328d16e1196edf0dbb7b5e3e83eec928d4a5
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 15:14:35 +0000

    Sat Jun 21 15:14:35 UTC 2025

--- test/CHK/test_checkout_solution.py
+++ test/CHK/test_checkout_solution.py
@@ -5,4 +5,23 @@ class TestCheckoutSolution:
     def test_multiple_basket_cases(self):
         basket = CheckoutSolution()
 
+        #test indv items
         assert basket.checkout("A") == 50
+        assert basket.checkout("B") == 30
+        assert basket.checkout("C") == 20
+        assert basket.checkout("D") == 15
+
+        #test A offers
+        assert basket.checkout("AAA") == 130
+        assert basket.checkout("AAAA") == 180
+        assert basket.checkout("AAAAA") == 230
+        assert basket.checkout("AAAAAA") == 260
+
+        #test B offers
+        assert basket.checkout("BB") == 45
+        assert basket.checkout("BBB") == 75
+
+        #test sum
+        assert basket.checkout("ABCD") == 50 + 30 + 20 + 15
+        #test two offers
+        assert basket.checkout("AAABBCD") == 130 + 45 + 20 + 15
