ae43a3494e38d548306c625ba29e21c7d90c2de8
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 14:29:18 +0000

    Sat Jun 21 14:29:18 UTC 2025

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -4,3 +4,7 @@ class TestHelloSolution:
     def test_msg(self):
         hello_msg = HelloSolution()
         assert hello_msg.hello("John") == "Hello, John!"
+
+    def test_not_null(self):
+        hello_msg = HelloSolution()
+        assert hello_msg.hello("John") is not None
