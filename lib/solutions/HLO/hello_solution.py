e1adb64553c23f6b2b82239e5cf34aea935f8787
sbx_user1051 <sbx_user1051@169.254.6.119>
2025-06-21 14:15:53 +0000

    Sat Jun 21 14:15:53 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,6 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-        msg = "Hello, World!"
+        msg = f"Hello, {friend_name}!"
 
         return msg
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -3,4 +3,4 @@ from lib.solutions.HLO.hello_solution import HelloSolution
 class TestHelloSolution:
     def test_msg(self):
         hello_msg = HelloSolution()
-        assert hello_msg.hello("Sam") == "Hello, World!"
+        assert hello_msg.hello("John") == "Hello, John!"
